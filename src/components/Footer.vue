ceb1bb81e7b24600496b5b9af56c83eacd4394c8
jawyoonis <awyoonisj@gmail.com>
2025-02-01 23:25:39 -0800

    added devices resize to this code

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,9 +3,14 @@
       <div class="container mx-auto text-center">
         <p class="text-lg font-semibold">EAST INSTITUTE</p>
         <div class="mt-4 flex justify-center space-x-6">
-          <a href="/contact" class="hover:text-gray-400">Contact</a>
-          <a href="/blog" class="hover:text-gray-400">Blog</a>
-          <a href="#" class="hover:text-gray-400">Curriculum</a>
+          <router-link to="/" class="hover:text-gray-400">Home</router-link>
+          <router-link to="/about" class="hover:text-gray-400">About</router-link>
+          <router-link to="/blog" class="hover:text-gray-400">Blog</router-link>
+          <router-link to="/waitlist" class="hover:text-gray-400">Waitlist</router-link>
+          <router-link to="/courses" class="hover:text-gray-400">Courses</router-link>
+          <router-link to="/curriculum" class="hover:text-gray-400">Curriculum</router-link>
+          <router-link to="/contact" class="hover:text-gray-400">Contact</router-link>
+          <router-link to="/AdminDashboard" class="hover:text-gray-400">Admin Dashboard</router-link>
         </div>
       </div>
     </footer>
