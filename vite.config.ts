fc8651fa4d8c91d1c723aa1415c23ba00027b133
jawyoonis <awyoonisj@gmail.com>
2025-02-01 22:57:02 -0800

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/east-institute/', // Replace with your GitHub repository name
   plugins: [
     vue(),
     vueDevTools(),
@@ -16,3 +16,4 @@ export default defineConfig({
     },
   },
 })
+
